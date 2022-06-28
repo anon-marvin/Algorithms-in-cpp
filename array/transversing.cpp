37cd3d1d16355c2c38874a811dc3b154aebf6d63
anon-marvin <anon.marvin.sohan@gmail.com>
2022-06-28 20:44:19 +0530

    Transvering algoritm corrected

--- array/transversing.cpp
+++ array/transversing.cpp
@@ -2,14 +2,12 @@
 using namespace std;
 int main()
 {
-   
-   int A[5]={3,6,4,8,9};
-   for(int i=0;i<5;i++){
-     cout<<A[i]<<endl;
-   }
-
-
-
-return 0;
+  int A[1000],n;
+  cout<<"Enter the size of the array : "<<endl;
+  cin>>n;
+  cout<<"Enter the elements of arrays :"<<endl;
+  for(int i=0;i<n;i++)
+    cin>>A[i];
+  return 0;
 }
 
